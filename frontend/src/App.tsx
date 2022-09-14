5fea66b6a663f6ba39c07e6562d373f59e81522f
Juvens St Louis <juvensstlouis77@gmail.com>
2022-09-14 16:43:42 -0300

    feat: enhance styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import "antd/dist/antd.css";
-import { Form, Input, Button, Alert } from "antd";
+import { Form, Input, Button, notification } from "antd";
 import ChurchRequestModel from "./models/ChurchRequestModel";
 import axios from "axios";
-import { useState } from "react";
 import "./index.css";
 import "./App.css";
 
 function App() {
-  const [success, setSuccess] = useState<boolean | null>(null);
-
   const validateMessages = {
-    required: "${label} deve ser informado(a)!",
+    required: "'${label}' deve ser informado(a)!",
+    pattern: {
+      mismatch: "'${label}' não está no formato correto!"
+    }
   };
 
   const onSubmit = (values: ChurchRequestModel) => {
@@ -20,83 +20,91 @@ function App() {
       .post("https:localhost:5001/church", values)
       .then(function (response) {
         if (response.status === 200) {
-          setSuccess(true);
+          notification.success({
+            message: "Cadastrado com sucesso",
+          });
+        } else {
+          throw new Error();
         }
       })
       .catch(function (error) {
-        setSuccess(false);
         console.log(error);
+
+        notification.error({
+          message: "Erro ao cadastrar",
+        });
       });
   };
 
+  const maskCep = (_cep: string | undefined) : string => {
+    if (!_cep) {
+      return "";
+    }
+    _cep = _cep.replace(/\D/g, "");
+    _cep = _cep.replace(/(\d{5})(\d)/, "$1-$2");
+    return _cep;
+  };
+
   return (
-    <Form
-      className="forms"
-      onFinish={onSubmit}
-      autoComplete="off"
-      validateMessages={validateMessages}
-    >
-      <Form.Item
-        label="Nome"
-        name="name"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
+    <div className="content">
+      <Form
+        className="forms"
+        onFinish={onSubmit}
+        autoComplete="off"
+        validateMessages={validateMessages}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Data de Fundação"
-        name="foundationDate"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input type="date" />
-      </Form.Item>
-      <Form.Item
-        label="CEP"
-        name="zipCode"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Número"
-        name="number"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input type="number" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-      {success === true && (
-        <Alert
-          message="Cadastrado com sucesso"
-          type="success"
-          showIcon
-          closable
-        />
-      )}
-      {success === false && (
-        <Alert message="Erro ao cadastrar" type="error" showIcon closable />
-      )}
-    </Form>
+        <Form.Item
+          label="Nome"
+          name="name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Data de Fundação"
+          name="foundationDate"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="date" />
+        </Form.Item>
+        <Form.Item
+          label="CEP"
+          name="zipCode"
+          rules={[
+            {
+              required: true,
+              pattern: new RegExp("[0-9]+")
+            },
+          ]}
+        >
+          <Input maxLength={8}/>
+        </Form.Item>
+        <Form.Item
+          label="Número"
+          name="number"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="number" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
 
